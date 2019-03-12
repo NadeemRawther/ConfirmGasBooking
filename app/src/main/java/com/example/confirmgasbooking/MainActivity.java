6dbc0e2e5a2941381bb63d14cdd44014f8f6fcb6
nadeem <nadeemsani786@hotmail.com>
2019-03-12 15:07:04 +0530

    Its my new commit

--- app/src/main/java/com/example/confirmgasbooking/MainActivity.java
+++ app/src/main/java/com/example/confirmgasbooking/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.confirmgasbooking;
 
 import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
 import android.app.PendingIntent;
 import android.content.ContentResolver;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -16,16 +20,18 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-
+Button acceptbutton,rejectbutton, reject;
+TextView messageforalert;
 
     private static MainActivity inst;
-
+    SmsManager smsManager = SmsManager.getDefault();
     private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 1;
     private static final String TAG = "Nadeem WAS";
     ListView messages;
@@ -34,56 +40,87 @@ public class MainActivity extends AppCompatActivity {
     private static final int READ_SMS_PERMISSIONS_REQUEST = 1;
 
     Button sendsms;
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-sendsms = (Button)findViewById(R.id.sendsms);
+        sendsms = (Button)findViewById(R.id.sendsms);
         messages = (ListView) findViewById(R.id.listview);
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.SEND_SMS) !=
-                PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
-            Log.d(TAG, "permission not granted");
-            // Permission not yet granted. Use requestPermissions().
-            // MY_PERMISSIONS_REQUEST_SEND_SMS is an
-            // app-defined int constant. The callback method gets the
-            // result of the request.
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.SEND_SMS},
-                    MY_PERMISSIONS_REQUEST_SEND_SMS);
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.READ_SMS},
-                    MY_PERMISSIONS_REQUEST_SEND_SMS);
-        } else {
-            // Permission already granted. Enable the message button.
-
-        }
+reject = (Button)findViewById(R.id.reject);
         arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, smsMessagesList);
         messages.setAdapter(arrayAdapter);
-        refreshSmsInbox();
-sendsms.setOnClickListener(new View.OnClickListener() {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            getPermissionToReadSMS();
+        } else {
+            refreshSmsInbox();
+        }
+
+
+
+
+        sendsms.setOnClickListener(new View.OnClickListener() {
+
+
+
+    @TargetApi(Build.VERSION_CODES.M)
     @Override
     public void onClick(View v) {
-        String scAddress = null;
-        // Set pending intents to broadcast
-        // when message sent and when delivered, or set to null.
-        PendingIntent sentIntent = null, deliveryIntent = null;
-        // Use SmsManager.
-        SmsManager smsManager = SmsManager.getDefault();
-        /*smsManager.sendTextMessage
-                (destinationAddress, scAddress, smsMessage,
-                        sentIntent, deliveryIntent);
-    */
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            getPermissionToReadSMS();
+        } else {
+            smsManager.sendTextMessage("9207419064", null, "Accepted", null, null);
+            Toast.makeText(MainActivity.this, "Message sent!", Toast.LENGTH_SHORT).show();
+        }
+        sendsms.setVisibility(View.INVISIBLE);
+        reject.setVisibility(View.INVISIBLE);
+
+
     }
 });
+        reject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendsms.setVisibility(View.INVISIBLE);
+                reject.setVisibility(View.INVISIBLE);
+
+            }
+        });
+
+
+
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void getPermissionToReadSMS() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (shouldShowRequestPermissionRationale(
+                    Manifest.permission.READ_SMS)) {
+                Toast.makeText(this, "Please allow permission!", Toast.LENGTH_SHORT).show();
+            }
+            requestPermissions(new String[]{Manifest.permission.READ_SMS},
+                    READ_SMS_PERMISSIONS_REQUEST);
+        }
+    }
+
+
+
+
+
     public void updateInbox(final String smsMessage) {
         arrayAdapter.insert(smsMessage, 0);
         arrayAdapter.notifyDataSetChanged();
+        sendsms.setVisibility(View.VISIBLE);
+        reject.setVisibility(View.VISIBLE);
+        methForAlertBox(smsMessage);
     }
     public void refreshSmsInbox() {
-
+        checkpermission();
         ContentResolver contentResolver = getContentResolver();
         Cursor smsInboxCursor = contentResolver.query(Uri.parse("content://sms/inbox"), null, null, null, null);
         int indexBody = smsInboxCursor.getColumnIndex("body");
@@ -101,6 +138,64 @@ sendsms.setOnClickListener(new View.OnClickListener() {
 
     }
 
+
+    public void methForAlertBox(String message){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        View mView = View.inflate(MainActivity.this,R.layout.alert_box_for_message,null);
+        acceptbutton = (Button)mView.findViewById(R.id.acceptbutton);
+        rejectbutton  = (Button)mView.findViewById(R.id.rejectbutton);
+        messageforalert = (TextView)mView.findViewById(R.id.messageforalert);
+
+        builder.setView(mView);
+
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+        acceptbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                smsManager.sendTextMessage("8075467438", null, "Accepted", null, null);
+                Toast.makeText(MainActivity.this, "Message sent!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        rejectbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.cancel();
+            }
+        });
+        messageforalert.setText(message);
+
+
+
+    }
+
+
+public void checkpermission(){
+    if (ContextCompat.checkSelfPermission(this,
+            Manifest.permission.SEND_SMS) !=
+            PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
+        Log.d(TAG, "permission not granted");
+        // Permission not yet granted. Use requestPermissions().
+        // MY_PERMISSIONS_REQUEST_SEND_SMS is an
+        // app-defined int constant. The callback method gets the
+        // result of the request.
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.SEND_SMS},
+                MY_PERMISSIONS_REQUEST_SEND_SMS);
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.READ_SMS},
+                MY_PERMISSIONS_REQUEST_SEND_SMS);
+    }else{
+        return;
+    }
+
+
+
+}
+
+
     public static MainActivity instance() {
         return inst;
     }
@@ -113,35 +208,20 @@ sendsms.setOnClickListener(new View.OnClickListener() {
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
-                if (permissions[0].equalsIgnoreCase(Manifest.permission.SEND_SMS)
-                        && grantResults[0] ==
-                        PackageManager.PERMISSION_GRANTED) {
-
-
-
-
-
-
-
-
-
-
-
-                    // Permission was granted.
-                } else {
-                    // Permission denied.
-                    Log.d(TAG,"permission failed");
-                    Toast.makeText(MainActivity.this,
-                            "permission failed",
-                            Toast.LENGTH_SHORT).show();
-                    // Disable the message button.
-
-                }
+        if (requestCode == READ_SMS_PERMISSIONS_REQUEST) {
+            if (grantResults.length == 1 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Read SMS permission granted", Toast.LENGTH_SHORT).show();
+                refreshSmsInbox();
+            } else {
+                Toast.makeText(this, "Read SMS permission denied", Toast.LENGTH_SHORT).show();
             }
+
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
-    }
+        }
+
 
 
 
